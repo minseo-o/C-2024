5cbc8f8aa289d8489f2ff5e2e4cb3961aee6bf66
drsavea <drsavea@DESKTOP-55GG08S>
2024-03-21 10:53:52 +0900

    초기 커밋.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld //패키지
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
